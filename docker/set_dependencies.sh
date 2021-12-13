fcde5187eb497e616c2e4c5ca294aa84c7293370
cbotek <botekchristophe@gmail.com>
2021-12-13 11:04:54 -0500

    Add extra conf to spark default

--- docker/set_dependencies.sh
+++ docker/set_dependencies.sh
@@ -3,14 +3,15 @@ set -e
 
 SPARK_HOME="/usr/local/spark"
 
-POSTGRESQL="org.postgresql:postgresql:42.2.23"
+POSTGRESQLV="42.2.23"
+POSTGRESQL="org.postgresql:postgresql:${POSTGRESQLV}"
 AWS_SDK="com.amazonaws:aws-java-sdk-bundle:1.11.901"
 AWS_HADOOP="org.apache.hadoop:hadoop-aws:3.3.1"
 FERLAB_SPARK31="bio.ferlab:datalake-spark31_2.12:0.1.11"
 MSSQL_JDBC="com.microsoft.sqlserver:mssql-jdbc:8.4.1.jre8"
 ADAL4J="com.microsoft.aad:adal4j:0.0.2"
 MSSQL_SPARK_CONNECTOR="com.microsoft.azure:spark-mssql-connector_2.12:1.2.0"
-DELTA_CORE="io.delta:delta-core_2.12:1.0.0"
+DELTA_CORE="io.delta:delta-core_2.12:1.1.0"
 
 packages="spark.jars.packages "
 
@@ -24,5 +25,6 @@ done
 packages+=${DELTA_CORE}
 
 echo ${packages} >> ${SPARK_HOME}/conf/spark-defaults.conf
+echo "spark.jars /opt/spark/jars/postgresql-${POSTGRESQLV}.jar" >> ${SPARK_HOME}/conf/spark-defaults.conf
 
 chown jovyan ${SPARK_HOME}/conf/spark-defaults.conf
