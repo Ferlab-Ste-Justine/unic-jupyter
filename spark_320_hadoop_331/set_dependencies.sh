3baac8f5fbb0b7112c55941bec65fa0f8808dc66
cbotek <cbotek@ferlab.bio>
2022-02-07 11:47:59 -0500

    bump lib to 0.2.14

--- spark_320_hadoop_331/set_dependencies.sh
+++ spark_320_hadoop_331/set_dependencies.sh
@@ -3,14 +3,16 @@ set -e
 
 SPARK_HOME="/usr/local/spark"
 
+FERLAB_SPARK31="bio.ferlab:datalake-spark31_2.12:0.2.15"
+
 POSTGRESQLV="42.2.23"
 HADOOPV="3.3.1"
 POSTGRESQL="org.postgresql:postgresql:${POSTGRESQLV}"
 AWS_SDK="com.amazonaws:aws-java-sdk-bundle:1.11.901"
+AWS_SOFTWARE_SDK="software.amazon.awssdk:s3:2.17.71"
 AWS_HADOOP="org.apache.hadoop:hadoop-aws:${HADOOPV}"
 HADOOP_COMMON="org.apache.hadoop:hadoop-common:${HADOOPV}"
 HADOOP_MR_CORE="org.apache.hadoop:hadoop-mapreduce-client-core:${HADOOPV}"
-FERLAB_SPARK31="bio.ferlab:datalake-spark31_2.12:0.2.9"
 MSSQL_ADAL4J="com.microsoft.aad:adal4j:0.0.2"
 MSSQL_JDBC="com.microsoft.sqlserver:mssql-jdbc:9.4.1.jre8"
 MSSQL_SPARK_CONNECTOR="com.microsoft.azure:spark-mssql-connector_2.12:1.2.0"
@@ -24,7 +26,7 @@ packages="spark.jars.packages "
 
 echo "Add spark.jars.package..."
 
-for package in ${POSTGRESQL} ${AWS_SDK} ${AWS_HADOOP} ${HADOOP_COMMON} ${HADOOP_MR_CORE} ${FERLAB_SPARK31} ${MSSQL_ADAL4J} ${MSSQL_JDBC} ${MSSQL_SPARK_CONNECTOR} ${MSSQL_MSAL4J} ${AZURE_CORE} ${AZURE_CORE_NETTY} ${AZURE_CORE_SECURITY}; do
+for package in ${POSTGRESQL} ${AWS_SDK} ${AWS_SOFTWARE_SDK} ${AWS_HADOOP} ${HADOOP_COMMON} ${HADOOP_MR_CORE} ${FERLAB_SPARK31} ${MSSQL_ADAL4J} ${MSSQL_JDBC} ${MSSQL_SPARK_CONNECTOR} ${MSSQL_MSAL4J} ${AZURE_CORE} ${AZURE_CORE_NETTY} ${AZURE_CORE_SECURITY}; do
     echo "Adding ${package}"
     packages+="${package},"
 done
