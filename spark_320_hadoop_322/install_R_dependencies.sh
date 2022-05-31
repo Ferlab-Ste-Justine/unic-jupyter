afc10b64e1acb4189581b3bf205a9bbb1b3d9b25
cbotek <cbotek@ferlab.bio>
2022-05-31 10:43:13 -0400

    Update R dependencies

--- spark_320_hadoop_322/install_R_dependencies.sh
+++ spark_320_hadoop_322/install_R_dependencies.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-Rscript -e 'install.packages("tidyverse", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("opalr", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("aws.s3", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("reticulate", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
