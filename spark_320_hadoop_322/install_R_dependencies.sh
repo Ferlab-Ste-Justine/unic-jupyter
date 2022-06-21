989890f82fc80568456959bf34599d06e6b5909d
cbotek <cbotek@ferlab.bio>
2022-06-21 09:35:06 -0400

    Add ggthemes and plotly in R dependencies

--- spark_320_hadoop_322/install_R_dependencies.sh
+++ spark_320_hadoop_322/install_R_dependencies.sh
@@ -5,3 +5,5 @@ Rscript -e 'install.packages("tidyverse", lib = "/usr/local/spark-3.2.0-bin-hado
 Rscript -e 'install.packages("opalr", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("aws.s3", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("reticulate", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("ggthemes", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("plotly", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
