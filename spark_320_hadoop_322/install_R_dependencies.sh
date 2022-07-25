c502d4ddaaef5045606799f5e18c079f3bb4f018
Christophe Botek <cbotek@ferlab.bio>
2022-07-25 11:22:51 -0400

    Change R dependency order

--- spark_320_hadoop_322/install_R_dependencies.sh
+++ spark_320_hadoop_322/install_R_dependencies.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-Rscript -e 'install.packages("tidyverse", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("opalr", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("aws.s3", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("reticulate", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("ggthemes", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("plotly", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("vctrs", version = "0.4.1", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("tidyverse", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
