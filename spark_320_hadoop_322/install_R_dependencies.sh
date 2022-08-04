c60e5432a5a75edcb80e44dbd4970b5bb6385d9c
Laura Bégin <lbegin@ferlab.bio>
2022-08-04 11:54:14 -0400

    Set reticulate version to 1.22

--- spark_320_hadoop_322/install_R_dependencies.sh
+++ spark_320_hadoop_322/install_R_dependencies.sh
@@ -6,6 +6,6 @@ Rscript -e 'install.packages("dplyr", version = "1.0.9", lib = "/usr/local/spark
 Rscript -e 'install.packages("tidyverse", version = "1.3.2", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("opalr", version = "3.1.1", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("aws.s3", version = "0.3.21", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
-Rscript -e 'install.packages("reticulate", version = "1.25", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("reticulate", version = "1.22", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("ggthemes", version = "4.2.4", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("plotly", version = "4.10.0", lib = "/usr/local/spark-3.2.0-bin-hadoop3.2/R/lib", repos = "http://cran.us.r-project.org")'
