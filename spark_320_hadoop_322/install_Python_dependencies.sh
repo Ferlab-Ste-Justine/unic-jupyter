b7f8acbf002ce46ffb9bef59e22278163245c72b
Laura Bégin <lbegin@ferlab.bio>
2022-09-06 15:24:43 -0400

    feat: Update pandas + Add new python packages

--- spark_320_hadoop_322/install_Python_dependencies.sh
+++ spark_320_hadoop_322/install_Python_dependencies.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-pip install pandas-profiling
+pip install pandas-profiling==3.2.0
+pip install gspread
+pip install df2gspread
+pip install pyreadr
+pip install gspread tabulate
